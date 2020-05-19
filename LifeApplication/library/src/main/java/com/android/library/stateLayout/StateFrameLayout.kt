f22c86aa28fd18ce2501b1117dfc87f3b41753e8
ydl <1006891880@qq.com>
2020-05-19 22:51:49 +0800

    StateLayout 11

--- LifeApplication/library/src/main/java/com/android/library/stateLayout/StateFrameLayout.kt
+++ LifeApplication/library/src/main/java/com/android/library/stateLayout/StateFrameLayout.kt
@@ -1,4 +1,44 @@
 package com.android.library.stateLayout
 
-class StateLayoutView {
+import android.content.Context
+import android.util.AttributeSet
+import android.util.Log
+import android.util.SparseArray
+import android.view.View
+import android.widget.FrameLayout
+
+
+class StateFrameLayout : FrameLayout {
+
+    constructor(context: Context): this(context,null){
+        Log.e("yyy","constructor  first  ")
+    }
+    constructor(context: Context,attributeSet: AttributeSet?):this(context,attributeSet,0){
+        Log.e("yyy","constructor  two  ")
+    }
+    constructor(context: Context,attributeSet: AttributeSet?,defStyleAtt:Int):super(context,attributeSet,defStyleAtt){
+        Log.e("yyy","constructor  three  ")
+    }
+
+    init {
+        Log.e("yyy","constructor  init  ")
+    }
+
+    companion object{
+        //loading  id
+       const val LAYOUT_LOADING_ID = 1
+        //content  id
+       const val LAYOUT_CONTENT_ID = 2
+        //exception id
+       const val LAYOUT_ERROR_ID = 3
+        //network error  id
+        const val LAYOUT_NETWORK_ERROR_ID = 4
+        //empty id
+        const val LAYOUT_EMPTY_DATA_ID = 5
+
+        //save list view
+        var layoutSparseArray = SparseArray<View>()
+
+    }
+
 }
--- LifeApplication/library/src/main/java/com/android/library/stateLayout/StateLayoutManager.kt
+++ LifeApplication/library/src/main/java/com/android/library/stateLayout/StateLayoutManager.kt
@@ -1,4 +1,25 @@
 package com.android.library.stateLayout
 
+import android.content.Context
+import android.view.ViewStub
+
+// 管理状态的管理器
 class StateLayoutManager {
+
+    var context: Context? = null
+    var netWorkErrorVs: ViewStub? = null  //网络异常的 viewStub
+    var netWorkErrorRetryViewId: Int = 0  //网络错误 重试
+    var emptyDataVs: ViewStub? = null   //空数据 viewStub
+    var emptyDataEetryViewId: Int = 0  //空数据  重试
+    var errorVs: ViewStub? = null     //errorVS
+    var errorRetryViewId: Int = 0   //error viewId
+    var loadingLayoutResId: Int = 0   //loading view id
+    var contentLayoutResId: Int = 0   //content view id
+    var retryViewId : Int = 0         //retry  view id
+    var emptyDataIconImageId : Int = 0  // empty data icon
+    var emptyDataTextTipId : Int = 0   // empty data tip text
+    var errorIconImageId : Int = 0    //error icon image
+    var errorTextTipId : Int = 0      // error tip text
+
+
 }
